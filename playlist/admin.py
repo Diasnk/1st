047880251bf991274cc13de8725581f155b9a48c
Diasnk <diasnugumanov258@gmail.com>
2021-11-13 19:07:51 +0600

    privet

--- playlist/admin.py
+++ playlist/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import *
 
 class ProfileModelAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "mail"]
+    list_display = ["id", "name", "listenings"]
     list_display_links = ["name"]
     search_fields = ["name"]
     class Meta:
-        model = Profile
+        model = Playlist
 
-admin.site.register(Profile, ProfileModelAdmin)
+admin.site.register(Playlist, ProfileModelAdmin)
